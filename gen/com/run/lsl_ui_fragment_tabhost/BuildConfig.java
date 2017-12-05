2ec0faabb45a080d6b81bb343e091d6bf965f7f9
linshunli <xiaolinzi12138@outlook.com>
2017-12-05 16:11:10 +0800

    安卓页面代码首次提交

---
+++ gen/com/run/lsl_ui_fragment_tabhost/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.run.lsl_ui_fragment_tabhost;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ gen/com/yanis/yc_ui_fragment_tabhost/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.yanis.yc_ui_fragment_tabhost;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
